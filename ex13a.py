18c81971def78e37e238925645931732f659dc89
Jagoda <malgonia.kulinska@gmail.com>
2018-04-16 19:41:07 +0100

    cleanup

--- ex13a.py
+++
@@ -1,5 +0,0 @@
-for a in range(-10,10):
-    if a < 5:
-        print "Ale zimno ", a, " stopni"
-    else:
-        print "Cieplo ", a ,"stopni"
--- ex2.py
+++
@@ -1,6 +0,0 @@
-# A comment, this is so you can read yuor progrma later.
-# Anything after the # is ignored by python.
-print "I could have code like this." # and the comments after this is ignored
-# You can also use a comment to "disable" or comment out a piece of code:
-# print "This wont't run."
-print "This will run."
